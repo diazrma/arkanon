b23f8e17b47cb33ed66ff2c4eb38322af2716b85
Rodrigo <diazrma@gmail.com>
2021-06-20 12:46:41 -0300

    add pwa

--- js/sw.js
+++ js/sw.js
@@ -1,15 +1,30 @@
 const cache_name = 'static-v1';
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(cache_name).then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-      ]);
-    })
-  )
-});
+addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;     // if valid response is found in cache return it
+          } else {
+            return fetch(event.request)     //fetch from internet
+              .then(function(res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());    //save the response for future
+                    return res;   // return the fetched data
+                  })
+              })
+              .catch(function(err) {       // fallback mechanism
+                return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                  .then(function(cache) {
+                    return cache.match('/offline.html');
+                  });
+              });
+          }
+        })
+    );
+  });          
 
 self.addEventListener('activate', function activator(event) {
   event.waitUntil(
