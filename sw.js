4cd86fa1110fc93e63663963322421f118150159
Rodrigo <diazrma@gmail.com>
2021-06-20 17:30:22 -0300

    button install pwa

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ var VERSION = '22';
 this.addEventListener('install', function(e) {
     e.waitUntil(caches.open(VERSION).then(cache => {
         return cache.addAll([  
-                       '/index.html'
+                       'index.html'
     
     
     ]);
