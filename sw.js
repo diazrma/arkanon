cfe36768b94f92ed949b9c0f82999bc043a10cb8
Rodrigo <diazrma@gmail.com>
2021-06-22 19:18:32 -0300

    test

--- sw.js
+++ sw.js
@@ -3,15 +3,12 @@ self.addEventListener('install', e => {
     e.waitUntil(
       // depois que o Service Worker estiver instalado,,
       // abra um novo cache
-      caches.open('my-pwa-cache').then(cache => {
+      caches.open('arkanon').then(cache => {
         // adicione todas as URLs de recursos que queremos armazenar em cache
         return cache.addAll([
           '/',
           '/index.html',
-          '/sobre.html',
-          '/images/logo.jpg',
-          '/styles/main.min.css',
-          '/scripts/main.min.js',
+          
         ]);
       })
     );
