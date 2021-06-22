7b4580233979e65e8188a7de3f40c1ed53266412
Rodrigo <diazrma@gmail.com>
2021-06-22 19:16:05 -0300

    test

--- sw.js
+++ sw.js
@@ -1,45 +1,18 @@
-var VERSION = '22';
-this.addEventListener('install', function(e) {
-    e.waitUntil(caches.open(VERSION).then(cache => {
-        return cache.addAll([  
-                       'index.html'
-    
-    
-    ]);
-    }))
-});
-this.addEventListener('fetch', function(e) {
-    var tryInCachesFirst = caches.open(VERSION).then(cache => {
-        return cache.match(e.request).then(response => {
-            if (!response) {
-                return handleNoCacheMatch(e);
-            }
-            fetchFromNetworkAndCache(e);
-            return response
-        });
-    });
-    e.respondWith(tryInCachesFirst);
-});
-this.addEventListener('activate', function(e) {
-    e.waitUntil(caches.keys().then(keys => {
-        return Promise.all(keys.map(key => {
-            if (key !== VERSION)
-                return caches.delete(key);
-        }));
-    }));
-});
-
-function fetchFromNetworkAndCache(e) {
-    if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
-    return fetch(e.request).then(res => {
-        if (!res.url) return res;
-        if (new URL(res.url).origin !== location.origin) return res;
-        return caches.open(VERSION).then(cache => {
-            return res;
-        });
-    }).catch(err => console.error(e.request.url, err));
-}
-
-function handleNoCacheMatch(e) {
-    return fetchFromNetworkAndCache(e);
-}
+// sw.js
+self.addEventListener('install', e => {
+    e.waitUntil(
+      // depois que o Service Worker estiver instalado,,
+      // abra um novo cache
+      caches.open('my-pwa-cache').then(cache => {
+        // adicione todas as URLs de recursos que queremos armazenar em cache
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/sobre.html',
+          '/images/logo.jpg',
+          '/styles/main.min.css',
+          '/scripts/main.min.js',
+        ]);
+      })
+    );
+   });
