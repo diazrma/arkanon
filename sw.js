9cb10730408badacc8c586224c86e30c86cd28b0
Rodrigo <diazrma@gmail.com>
2021-06-22 19:45:31 -0300

    test

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
-// sw.js
-self.addEventListener('install', e => {
-    e.waitUntil(
-      // depois que o Service Worker estiver instalado,,
-  
-      caches.open('arkanon').then(cache => {
+var cache_name = 'arkanon';
 
-        return cache.addAll([
-          '/sw.js',
-          '/index.html',
-          
-        ]);
-      })
-    );
-   });
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cache_name).then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/styles.css',
+        '/manifest.js',
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(cache_name) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
