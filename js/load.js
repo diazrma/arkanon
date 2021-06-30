4738ebfa3fed3ccfe144e7af7b9c58a61b2a0f61
Rodrigo <diazrma@gmail.com>
2021-06-29 22:00:16 -0300

    add loading

--- js/load.js
+++ js/load.js
@@ -7,6 +7,5 @@ document.write(loading);
 document.getElementById("grid").classList.add('d-none');
 
 setTimeout(function () {
-    
     document.getElementById("spinner").classList.add('d-none');
 }, 600);
